3d6a940b2615ca6390a83c53251af5474c76d2c3
moath al-abdulrazaq <moath@moaths-air.home>
2022-08-24 20:58:19 +0300

    1st

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+display: flex;
+padding: 20px;
+    
+`
+
+const CategoryItem = ({item}) => {
+  return (
+    <div>
+        <Container>
+           hello
+        </Container>
+    </div>
+  )
+}
+
+export default CategoryItem
