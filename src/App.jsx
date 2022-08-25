7d0261851d696e3d8b6712c16888d4bc0e6281c5
moath al-abdulrazaq <moath@moaths-air.home>
2022-08-25 18:00:35 +0300

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Home from "./pages/Home";
 import React from "react";
+import ProductList from "./pages/ProductList";
+// import Product from "./components/Product";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = () => {
   return <div>
-    <Home/>
+    <Register/>
     </div>;
 };
 
